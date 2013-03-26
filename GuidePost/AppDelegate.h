a2cf9c0115f4edfa5df17bb55a62a5e58582cecc
mmiyaji <ruhenheim1223@gmail.com>
2013-03-27 02:05:42 +0900

    Initial Commit

---
+++ GuidePost/AppDelegate.h
@@ -0,0 +1,19 @@
+//
+//  AppDelegate.h
+//  GuidePost
+//
+//  Created by Masahiro MIYAJI on 3/27/13.
+//  Copyright (c) 2013 Masahiro MIYAJI. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class ViewController;
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (strong, nonatomic) ViewController *viewController;
+
+@end
---
+++ GuidePost/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  GuidePost
+//
+//  Created by Masahiro MIYAJI on 3/27/13.
+//  Copyright (c) 2013 Masahiro MIYAJI. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
